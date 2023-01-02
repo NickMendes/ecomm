a5082a87a010d86beba7a82828b630c99e426ad2
Nick <marina@sirq.com.br>
2023-01-02 11:02:57 -0300

    Create Function createUserUseCase

--- account/use-case/createUserAccount.js
+++ account/use-case/createUserAccount.js
@@ -1,1 +1,20 @@
+const usuariosList = [];
 
+const createUserUseCase = (name, email, password) => {
+  const dateNow = new Date().toJSON().slice(0,10).replace(/-/g,'-');
+  const date = dateNow.toString('dd-MM-yyyy');
+
+  const user = {
+    id: usuariosList.length + 1,
+    name,
+    email,
+    password,
+    createDate: date
+  };
+
+  usuariosList.push(user);
+  
+  return user;
+}
+
+export default createUserUseCase;
