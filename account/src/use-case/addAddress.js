8f138e6cda205eab0170cdb92e0f0603a7127005
Nick <marina@sirq.com.br>
2023-01-11 10:22:50 -0300

    Refactor: IF on addAddress.js

--- account/src/use-case/addAddress.js
+++ account/src/use-case/addAddress.js
@@ -4,14 +4,15 @@ export const addAddressUseCase = (add, email) =>{
   const userOld = userList.find((ele) => ele.email === email);
   const index = userList.findIndex((ele) => ele.email === email);
   
-  if (userOld) {
-    const newUser = { ...userOld, address: add };
-    userList.splice(index, 1);
-    userList.push(newUser);
-    return true;
-  } else if (!userOld) {
+  if (!userOld) {
     return false;
-  };
+  } 
+
+  const newUser = { ...userOld, address: add };
+  userList.splice(index, 1);
+  userList.push(newUser);
+
+  return true;
 };
 
 export { userList };
