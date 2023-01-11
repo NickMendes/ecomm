2a294b0ccad55f53f8f0cd0edc308254c5f5324f
Nick <marina@sirq.com.br>
2023-01-11 10:49:32 -0300

    Refactor.test: new user test with address

--- account/test/createUserAccount.test.js
+++ account/test/createUserAccount.test.js
@@ -1,7 +1,20 @@
 import { createUserUseCase, userList } from '../src/use-case/createUserAccount.js';
 
-const user1 = createUserUseCase('Maria', 'maria@gmail.com', 'maria123');
-const user2 = createUserUseCase('Jose', 'jose@hotmail.com', '123456js');
+//Before Each
+const user1 = createUserUseCase('Maria', 'maria@exemple.com', 'maria123');
+const user2 = createUserUseCase('Jose', 'jose@exemple.com', '123456js');
+const address = {
+  logradouro: 'Rua Um',
+  numero: 100,
+  complemento: 'NP',
+  bairro: 'Qualquer',
+  CEP: 30100000,
+  cidade: 'São Paulo',
+  UF: 'SP'
+};
+const user3 = createUserUseCase('Fulana', 'fulana@example.com', '123fulana', address)
 
-console.log(user1, user2);
-console.log(userList);
+console.log('Insert new user, expect user object as response:\n', user1);
+console.log('Insert new user, expect user object as response:\n', user2);
+console.log('Insert new user, expect user object as response:\n', user3);
+console.log('Check userList, expect array with 3 users/objects:\n', userList);
