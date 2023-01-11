c490bde40f551663b1c8d62c0db2f57c8ad9a7f8
Nick <marina@sirq.com.br>
2023-01-11 10:35:23 -0300

    Refactor: Change IF and "ele" on find function

--- account/src/use-case/searchUserAccountByEmail.js
+++ account/src/use-case/searchUserAccountByEmail.js
@@ -1,11 +1,11 @@
 import { userList } from "./createUserAccount.js";
 
 export const searchUserAccountByEmailUseCase = (email) => {
-  const user = userList.find((ele) => ele.email === email);
+  const userFound = userList.find((user) => user.email === email);
   
-  if (user) {
-    return user
-  } else if (user === undefined) {
+  if (!userFound) {
     return Error('Usuário não encontrado');
   }
+
+  return userFound;
 };
