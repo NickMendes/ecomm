5ef362ea16b33599bd503c9bfb9f2ebbefc26a66
Nick <marina@sirq.com.br>
2023-01-11 10:31:44 -0300

    Refactor: change IF ans "ele" on find function

--- account/src/use-case/removeUserAccount.js
+++ account/src/use-case/removeUserAccount.js
@@ -1,13 +1,15 @@
 import { userList } from "./createUserAccount.js";
 
 export const removeUserUseCase = (email) => {
-  const index = userList.findIndex((ele) => ele.email === email);
+  const index = userList.findIndex((user) => user.email === email);
+  
   if (index === -1) {
     return false;
-  } else {
-    userList.splice(index, 1);
-    return true;
-  }
+  } 
+    
+  userList.splice(index, 1);
+  
+  return true;
 };
 
 export { userList };
