d7c6ba4eb49d010f73bdc15816e2b7646b0eb74f
Nick <marina@sirq.com.br>
2023-01-11 11:24:59 -0300

    Refactor: fix problem of search when list have people without address

--- account/src/use-case/findByUF.js
+++ account/src/use-case/findByUF.js
@@ -1,7 +1,7 @@
 import { userList } from "./createUserAccount.js";
 
 export const findByEstate = (uf) => {
-  const usuariosByState = userList.filter((user) => user.address.UF === uf);
+  const usuariosByState = userList.filter((user) => user.address?.UF === uf);
   
   if (!usuariosByState) {
     return Error('Não existem usuários desse estado');
