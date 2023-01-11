127ad21ab1c042a51865a14ce8fa3e0019eefd5e
Nick <marina@sirq.com.br>
2023-01-11 10:56:44 -0300

    Refactor.test: add users before test and change text to english

--- account/test/searchUserAccountByEmail.test.js
+++ account/test/searchUserAccountByEmail.test.js
@@ -1,7 +1,21 @@
 import { searchUserAccountByEmailUseCase } from "../src/use-case/searchUserAccountByEmail.js";
+import { createUserUseCase } from '../src/use-case/createUserAccount.js';
 
-const emailCorreto = searchUserAccountByEmailUseCase('rafa@el.com.br');
+createUserUseCase('Maria', 'maria@exemple.com', 'maria123');
+createUserUseCase('Jose', 'jose@exemple.com', '123456js');
+const address = {
+  logradouro: 'Rua Um',
+  numero: 100,
+  complemento: 'NP',
+  bairro: 'Qualquer',
+  CEP: 30100000,
+  cidade: 'São Paulo',
+  UF: 'SP'
+};
+createUserUseCase('Fulana', 'fulana@example.com', '123fulana', address);
+
+const emailCorreto = searchUserAccountByEmailUseCase('maria@exemple.com');
 const emailErrado = searchUserAccountByEmailUseCase('nao@email.com');
 
-console.log('Buscando email correto, espera um objeto como resposta', emailCorreto);
-console.log('Buscando email errado, espera msg de erro', emailErrado);
+console.log('Search for correct email, expects object with user:\n', emailCorreto);
+console.log('Search for incorrect email, expect Error Msg:\n', emailErrado);
