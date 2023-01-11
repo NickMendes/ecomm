30dd986b0a240c2cb1e4517f8ffd07507acbe1c5
Nick <marina@sirq.com.br>
2023-01-11 10:30:28 -0300

    Refactor: change IF and "ele" on filter function

--- account/src/use-case/findByUF.js
+++ account/src/use-case/findByUF.js
@@ -1,10 +1,11 @@
 import { userList } from "./createUserAccount.js";
 
 export const findByEstate = (uf) => {
-  const usuariosByState = userList.filter((ele) => ele.address.UF === uf);
-  if (usuariosByState) {
-    return usuariosByState;
-  } else if (!usuariosByState) {
+  const usuariosByState = userList.filter((user) => user.address.UF === uf);
+  
+  if (!usuariosByState) {
     return Error('Não existem usuários desse estado');
-  };
+  }
+  
+  return usuariosByState;
 };
