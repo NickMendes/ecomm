723a3877c24c07722b2def9699a085fc1601755c
Nick <marina@sirq.com.br>
2023-02-01 11:23:25 -0300

    Feat: Model Users Requirements

--- account/src/models/UserModel.js
+++ account/src/models/UserModel.js
@@ -3,20 +3,46 @@ import mongoose from 'mongoose';
 const userSchema = new mongoose.Schema(
   {
     id: { type: String },
-    name: { type: String, required: true },
-    email: { type: String, required: true },
-    password: { type: String, required: true },
-    cpf: { type: Number, required: true },
-    phones: { type: Array, required: true },
+    name: { 
+      type: String,
+      required: true
+    },
+    email: { 
+      type: String,
+      match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+      required: true
+    },
+    password: { 
+      type: String,
+      match: /^(?=.{8,32}$)(?=.*[a-z]).*\d.*\D/i,
+      required: true
+    },
+    cpf: { 
+      type: Number,
+      min: 10000000000,
+      max: 99999999999,
+      required: true
+    },
+    phones: { 
+      type: [{ type: Number, min: 1000000000, max: 9999999999999}]
+    },
     address: { 
-      cep: Number,
-      street: String,
-      number: Number,
+      cep: { type: Number, min: 10000000, max:99999999, required: true },
+      street: { type: String, required: true },
+      number: { type: String, required: true },
       complement: String,
-      city: String,
-      state: String
+      city: { type: String, required: true },
+      state: {
+        type: String,
+        enum: ["AC", "AL", "AM", "AP", "BA", "CE", "DF", "ES", "GO",
+        "MA", "MG", "MS", "MT", "PA", "PB", "PE", "PI", "PR", "RJ",
+        "RN", "RO", "RR", "RS", "SC", "SE", "SP", "TO"]
+      }
      },
-     create_date: { type: Date, default: Date.now },
+     creation_date: { 
+      type: Date,
+      default: Date.now
+    },
   }
 );
 
