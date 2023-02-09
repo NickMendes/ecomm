79a83dea1d0b6f53a68f1852ee4ccae5d34d6ea1
Nick <marina@sirq.com.br>
2023-02-09 14:47:53 -0300

    Feat: Models to Address, Sale and Cupoms

---
+++ finance/api/models/0004-cupoms.js
@@ -0,0 +1,35 @@
+'use-strict'
+
+module.exports = (sequelize, DataTypes) => {
+  const Cupoms = sequelize.define('Cupoms', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      validate: {
+        len: [5, 40]
+      }
+    },
+    cpf: {
+      type: DataTypes.STRING,
+      validate: {
+        isNumeric: true,
+        len: [11]
+      }
+    },
+  });
+
+  Cupoms.associate = function(models) {
+    Cupoms.belongsTo(models.Addresses, {
+      foreignKey: 'address_id'
+    })
+    Cupoms.belongsTo(models.Sales, {
+      foreignKey: 'sale_id'
+    })
+  }
+  
+  return Cupoms;
+};
