abfdcfa87fddfc8a034c82a24241564b87786a45
Nick <marina@sirq.com.br>
2023-02-09 14:49:00 -0300

    Feat: API ROUTE to Address, Sale and Cupoms

--- finance/api/api.js
+++ finance/api/api.js
@@ -5,5 +5,8 @@ const app = express()
 app.use(express.json())
 
 app.use('/payments', routes.paymentRoute);
+app.use('/address', routes.addressRoute);
+app.use('/sale', routes.saleRoute);
+app.use('/cupom', routes.cupomRoute);
 
 module.exports = app
