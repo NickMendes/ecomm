ba92cf698119f213577e824e3a59698111a2e60c
Nick <marina@sirq.com.br>
2023-02-10 14:22:12 -0300

    Fix: Take off unnecessary code

--- finance/api/server.js
+++ finance/api/server.js
@@ -2,8 +2,4 @@ const app = require('./api');
 
 const PORT = process.env.API_PORT || 3000;
 
-app.get('/', (_request, response) => {
-  response.send();
-});
-
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}`));
