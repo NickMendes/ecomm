15b99c564815c6a502c681db37e08c2ad35f91b3
Nick <marina@sirq.com.br>
2023-02-15 10:02:39 -0300

    Feat: WIP Payment controller

---
+++ finance-mongo/src/controllers/PaymentController.js
@@ -0,0 +1,62 @@
+import paymentModel from '../models/PaymentModel.js';
+
+class PaymentController {
+  static getAllPayments = (_req, res) => {
+    paymentModel.find((err, payment) => {
+      res.status(200).json(payment);
+    });
+  };
+
+  static getPaymentById = (req, res) => {
+    const id = req.params.id;
+
+    paymentModel.findById(id)
+      .populate('category_id') 
+      .exec((err, payment) => {
+        if(err) {
+          res.status(400).send({ message: err.message });
+        } else {
+          res.status(200).json(payment);
+        }
+      });
+  };
+
+  static createPayment = (req, res) => {
+    let payment = new paymentModel({ ...req.body, status: "CRIADO" });
+    
+    payment.save((err) => {
+      if(err) {
+        res.status(500).send({ message: err.message });
+      } else {
+        res.status(201).json(payment.toJSON());
+      }
+    });
+
+  };
+
+  static updatePayment = (req, res) => {
+    const id = req.params.id;
+
+    paymentModel.findByIdAndUpdate(id, { $set: req.body }, (err) => {
+      if(!err) {
+        res.status(200).json({ message: 'Payment updated success'});
+      } else {
+        res.status(500).send({ message: err.message });
+      }
+    });
+  };
+
+  static deletePayment = (req, res) => {
+    const id = req.params.id;
+
+    paymentModel.findByIdAndDelete(id, (err) => {
+      if(!err) {
+        res.status(200).json({ message: 'Payment deleted success'});
+      } else {
+        res.status(500).send({ message: err.message });
+      }
+    });
+  };
+}
+
+export default PaymentController;
