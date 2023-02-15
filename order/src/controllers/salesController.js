e37a5cb26fc493d7d97781cb212adbc10cd4bc66
Nick <marina@sirq.com.br>
2023-02-15 13:55:42 -0300

    Feat: Order CRUD complete

--- order/src/controllers/salesController.js
+++ order/src/controllers/salesController.js
@@ -3,7 +3,65 @@ import salesModel from '../model/salesModel.js';
 class SaleController {
   static getAllSales = (_req, res) => {
     salesModel.find((err, sale) => {
-      res.status(200).json(sale);
+      if(!err) {
+        res.status(200).json(sale);
+      } else {
+        res.status(500).send({ message: err.message });
+      }
+    });
+  };
+
+  static getSaleById = (req, res) => {
+    const id = req.params.id;
+
+    salesModel.findById(id, (err, sale) => {
+      if(err) {
+        res.status(400).send({ message: err.message });
+      } else {
+        res.status(200).send(sale);
+      }
+    });
+  };
+
+  // Esta gerando um _id para a ordem que tem dentro do sale não sei porque, mas não atrapalha meu código. Olhar depois como tira isso
+  static createSale = (req, res) => {
+    const saleInfo = req.body;
+    const total_price = saleInfo.order.reduce((acc, cur) =>  {
+      return acc + ((cur.product_price * cur.product_qty) - cur.discount)
+    }, 0);
+
+    let sale = new salesModel({ ...saleInfo, total_price });
+
+    sale.save((err) => {
+      if(err) {
+        res.status(500).send({ message: err.message });
+      } else {
+        res.status(201).send(sale.toJSON());
+      }
+    });
+  };
+
+  static updateSale = (req, res) => {
+    const id = req.params.id;
+
+    salesModel.findByIdAndUpdate(id, { $set: req.body }, (err) => {
+      if(!err) {
+        res.status(200).send({ message: 'Sale updated success' });
+      } else {
+        res.status(500).send({ message: err.message });
+      }
+    });
+  };
+
+  static deleteSale = (req, res) => {
+    const id = req.params.id;
+
+    salesModel.findByIdAndDelete(id, (err) => {
+      if(!err) {
+        res.status(200).send({ message: 'Sale deleted success' });
+      } else {
+        res.status(500).send({ message: err.message });
+      }
     });
   };
 };
--- order/src/routes/saleRoute.js
+++ order/src/routes/saleRoute.js
@@ -4,6 +4,10 @@ import SaleController from '../controllers/salesController.js';
 const router = express.Router();
 
 router
-  .get('/sale', SaleController.getAllSales);
+  .get('/sale', SaleController.getAllSales)
+  .get('/sale/:id', SaleController.getSaleById)
+  .post('/admin/sale', SaleController.createSale)
+  .patch('/admin/sale/:id', SaleController.updateSale)
+  .delete('/admin/sale/:id', SaleController.deleteSale);
 
 export default router;
