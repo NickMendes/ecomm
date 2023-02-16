768191263d72f35b54332e355d509a3f94993fae
Nick <marina@sirq.com.br>
2023-02-16 14:15:30 -0300

    Fix: payment route to singular

--- finance-mongo/src/routes/PaymentRoute.js
+++ finance-mongo/src/routes/PaymentRoute.js
@@ -4,10 +4,10 @@ import PaymentController from '../controllers/PaymentController.js';
 const router = express.Router();
 
 router
-  .get('/payments', PaymentController.getAllPayments)
-  .get('/payments/:id', PaymentController.getPaymentById)
-  .post('/admin/payments', PaymentController.createPayment)
-  .patch('/admin/payments/:id', PaymentController.updatePayment)
-  .delete('/admin/payments/:id', PaymentController.deletePayment);
+  .get('/payment', PaymentController.getAllPayments)
+  .get('/payment/:id', PaymentController.getPaymentById)
+  .post('/admin/payment', PaymentController.createPayment)
+  .patch('/admin/payment/:id', PaymentController.updatePayment)
+  .delete('/admin/payment/:id', PaymentController.deletePayment);
 
 export default router;
