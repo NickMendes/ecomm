5a7fb1c54d54bb9843d0399f82d53360c7149279
Nick <marina@sirq.com.br>
2023-02-16 11:32:01 -0300

    Feat: Cupom route

--- finance-mongo/src/routes/index.js
+++ finance-mongo/src/routes/index.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import paymentRoute from './PaymentRoute.js'
+import paymentRoute from './PaymentRoute.js';
+import cupomRoute from './CupomRoute.js';
 
 const routes = (app) => {
-  app.use(express.json(), paymentRoute);
+  app.use(express.json(), paymentRoute, cupomRoute);
 }
 
-export default routes
+export default routes;
