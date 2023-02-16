f61231f27e3f782f71c012002c45141f9970a9cb
Nick <marina@sirq.com.br>
2023-02-16 14:15:05 -0300

    Feat: cupom model payment_info addes

--- finance-mongo/src/models/CupomModel.js
+++ finance-mongo/src/models/CupomModel.js
@@ -46,17 +46,41 @@ const salesSubSchema = new mongoose.Schema(
   }
 );
 
+const paymentSubSchema = new mongoose.Schema(
+  {
+    name: { 
+      type: String,
+      minLength: 5,
+      match: /^[a-zA-Z][a-zA-Z.,$; ]+$/,
+      required: true
+    },
+    card_number: {
+      type: String,
+      minLength: 16,
+      maxLength: 16,
+      require: true 
+    },
+    expiration_date: {
+      type: String,
+      match: /([\d]{4}-(0[1-9]|10|11|12)$)/,
+      required: true
+    },
+    status: { 
+      type: String,
+      enum: ['CONFIRMADO', 'CRIADO'],
+      required: true
+    },
+    _id: false
+  }
+)
+
 const cupomSchema = new mongoose.Schema(
   {
     id: { type: String },
-    user_info: { userSubSchema },
-    sale_info: { salesSubSchema },
-    payment_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'payments',
-      required: true
-    }
-  }
+    user_info: userSubSchema,
+    sale_info: salesSubSchema,
+    payment_info: paymentSubSchema
+  }, { versionKey: false }
 );
 
 const cupoms = mongoose.model('cupoms', cupomSchema);
