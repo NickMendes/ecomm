4cefe76ceef97f644ba19e12eb4224735515e167
Nick <marina@sirq.com.br>
2023-03-07 15:37:49 -0300

    feat: login controller

--- account/src/controllers/UserController.js
+++ account/src/controllers/UserController.js
@@ -1,6 +1,25 @@
 import users from '../models/UserModel.js';
+import JWTHelp from '../helpers/token.js';
+import Hash from '../helpers/hashing.js';
 
 class UserController {
+  static login = async (req, res) => {
+    const { email, password } = req.body;
+
+    const checkUser = await users.findOne({ email });
+
+    const checkPassword = Hash.dehashing(password, checkUser.password);
+    
+    if (!checkUser || !checkPassword) {
+      return res.status(400).json({ message: 'Email or Password invalid' });
+    }
+    
+    const token = JWTHelp.create({ id: checkUser.id });
+
+    res.set('Authorization', token);
+    res.status(204).send();
+  };
+
   static getAllUsers = (_req, res) => {
     users.find((err, user) => {
       if(err) {
@@ -23,8 +42,10 @@ class UserController {
     });
   };
 
-  static createUser = (req, res) => {
-    let user = new users(req.body);
+  static createUser = async (req, res) => {
+    const hash = await Hash.hashing(req.body.password);
+
+    let user = new users({ ...req.body, password: hash });
 
     user.save((err) => {
       if(err) {
@@ -60,4 +81,4 @@ class UserController {
   };
 }
 
-export default UserController
+export default UserController;
