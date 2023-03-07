3c2aacd2d3e5f1f55e53a02ec6e97868539c3a08
Nick <marina@sirq.com.br>
2023-03-07 15:40:18 -0300

    feat: add middlewares and route login

--- account/src/routes/UserRoute.js
+++ account/src/routes/UserRoute.js
@@ -1,13 +1,16 @@
 import express from 'express';
 import UserController from '../controllers/UserController.js';
+import validateAccount from '../middleware/userMW.js';
+import tokenVal from '../middleware/authorizationMW.js';
 
 const router = express.Router();
 
 router
   .get('/user', UserController.getAllUsers)
   .get('/user/:id', UserController.getUserById)
-  .post('/admin/user', UserController.createUser)
-  .put('/admin/user/:id', UserController.updateUser)
+  .post('/admin/user', tokenVal, validateAccount, UserController.createUser)
+  .put('/admin/user/:id', tokenVal, UserController.updateUser)
   .delete('/admin/user/:id', UserController.deleteUser)
+  .post('/user/login', UserController.login)
 
 export default router;
