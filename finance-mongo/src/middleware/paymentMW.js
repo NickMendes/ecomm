c68be5cc4b0759393eec3271ab24d2b917c5b013
Nick <marina@sirq.com.br>
2023-03-08 16:42:18 -0300

    fix: merge conflicts

--- finance-mongo/src/middleware/paymentMW.js
+++ finance-mongo/src/middleware/paymentMW.js
@@ -5,7 +5,7 @@ const validatePayment = (req, res, next) => {
         value: Joi.number().min(1).required(),
         name: Joi.string().min(5).required(),
         card_number: Joi.string().creditCard().required(),
-        expiration_date: Joi.string().pattern(/[\d]{4}-(0[1-9]|10|11|12)$)/).required(),
+        expiration_date: Joi.string().pattern(/([\d]{4}-(0[1-9]|10|11|12)$)/).required(),
         cvv: Joi.string().pattern(/[\d]{3}/).required(),
         status: Joi.string().valid('CRIADO', 'CONFIRMADO', 'CANCELADO')
     });
