42417862c548b4c5f5911bc50cf9ce6134a63f18
Nick <marina@sirq.com.br>
2023-03-08 13:21:09 -0300

    feat: add middlewares to routes

--- product/src/routes/ProductRoute.js
+++ product/src/routes/ProductRoute.js
@@ -1,13 +1,15 @@
 import express from 'express';
 import ProductController from '../controllers/ProductController.js';
+import validadeProduct from '../middleware/productMW.js';
+import tokenVal from '../middleware/authorizationMW.js';
 
 const router = express.Router();
 
 router
   .get('/product', ProductController.getAllProducts)
   .get('/product/:id', ProductController.getProductById)
-  .post('/admin/product', ProductController.createProduct)
-  .put('/admin/product/:id', ProductController.updateProduct)
-  .delete('/admin/product/:id', ProductController.deleteProduct)
+  .post('/admin/product', tokenVal, validadeProduct, ProductController.createProduct)
+  .put('/admin/product/:id', tokenVal, ProductController.updateProduct)
+  .delete('/admin/product/:id', tokenVal, ProductController.deleteProduct)
 
 export default router;
