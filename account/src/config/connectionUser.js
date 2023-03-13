8eb0c9b6c0b877d97eb557163aa459df0667cdf9
Nick <marina@sirq.com.br>
2023-03-13 16:27:05 -0300

    Fix: remove server connection direct text

--- account/src/config/connectionUser.js
+++ account/src/config/connectionUser.js
@@ -3,7 +3,7 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-mongoose.connect('mongodb+srv://NickAdmin:Password123@cluster0.y6erwvt.mongodb.net/ecomm-user' || process.env.CONNECTION_STRING);
+mongoose.connect(process.env.CONNECTION_STRING);
 
 let db = mongoose.connection;
 
