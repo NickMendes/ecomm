700b1e5545b7cacab41e376ded6b9ca67db76341
Nick <marina@sirq.com.br>
2023-03-17 13:40:10 -0300

    refactor: test to ajust JWT

--- order/src/test/routes/SalesRoute.test.js
+++ order/src/test/routes/SalesRoute.test.js
@@ -1,5 +1,6 @@
 import request from 'supertest';
 import { describe, expect, it, afterEach, beforeEach } from '@jest/globals';
+import axios from 'axios';
 import app from '../../app.js';
 
 let server;
@@ -12,26 +13,34 @@ afterEach(() => {
     server.close();
 });
 
+let token;
 describe('GET route /sale', () => {
     it('Should return an array of sales', async () => {
+        const getToken = await axios.post(
+            'http://localhost:3002/user/login',
+            { email: 'test@test.com.br', password: 'test123$' });
+        token = getToken.headers['authorization'];
+
         const response = await request(app)
             .get('/sale')
+            .set('Authorization', token)
             .expect(200);
         expect(response.body[0].user_info.name).toEqual('João Castor da Silva');
     });
 });
 
 let idResponse;
-describe('POST route /admin/sale', () => {
+describe('POST route /sale', () => {
     it('Should add a new sale', async () => {
         const response = await request(app)
-            .post('/admin/sale')
+            .post('/sale')
+            .set('Authorization', token)
             .send({
-                user_id: '63ee835b5ab79d1681d8a05c',
+                user_id: '64078151aa777592686a3a90',
                 delivery_address: {
                     cep: '32017-756',
                     street: 'Rua das Acásias',
-                    number: 1792,
+                    number: '1792',
                     complement: 'apto 103'
                 },
                 order: [{
@@ -46,9 +55,23 @@ describe('POST route /admin/sale', () => {
 
     it('Should return error if body is empty', async () => {
         await request(app)
-            .post('/admin/product')
+            .post('/sale')
+            .set('Authorization', token)
             .send({})
-            .expect(400);
+            .expect(422);
+    });
+
+    it('Should return 401 if token is missing', async () => {
+        await request(app)
+            .post('/sale')
+            .expect(401);
+    });
+
+    it('Should return 401 if token is not valid', async () => {
+        await request(app)
+            .post('/sale')
+            .set('Authorization', 'qualquercoisaquenaosejaoauthorizadordeverdade')
+            .expect(401);
     });
 });
 
@@ -56,30 +79,40 @@ describe('GET route /sale/:id', () => {
     it('Should return an object of a sale', async () => {
         const response = await request(app)
             .get(`/sale/${idResponse}`)
+            .set('Authorization', token)
             .expect(200);
         expect(response.body.user_info.name).toEqual('João Castor da Silva');
     });
 });
 
-describe('PUT route /admin/sale/:id', () => {
+describe('PATCH route /sale/:id', () => {
     it('Should update information on the selected sale', async () => {
         await request(app)
-            .put(`/admin/sale/${idResponse}`)
+            .patch(`/sale/${idResponse}`)
+            .set('Authorization', token)
             .send({
-                cep: '123569-587'
+                delivery_address: {
+                    cep: '12356-587',
+                    street: 'Rua das Acásias',
+                    number: '1792',
+                    complement: 'apto 103'
+                }
             })
-            .expect(202);
+            .expect(204);
+        
         const response = await request(app)
             .get(`/sale/${idResponse}`)
+            .set('Authorization', token)
             .expect(200);
-        expect(response.body.delivery_address.cep).toEqual('123569-587');
+        expect(response.body.delivery_address.cep).toEqual('12356-587');
     });
 });
 
-describe('DELETE route /admin/sale/:id', () => {
+describe('DELETE route /sale/:id', () => {
     it('Should delete the selected sale', async () => {
         await request(app)
-            .delete(`/admin/sale/${idResponse}`)
-            .expect(202);
+            .delete(`/sale/${idResponse}`)
+            .set('Authorization', token)
+            .expect(204);
     });
 });
