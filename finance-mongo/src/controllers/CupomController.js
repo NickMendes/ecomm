e82e8356d0d96c26b44d450342c569f75cdb99de
Nick <marina@sirq.com.br>
2023-03-17 16:57:18 -0300

    refactor: JWT authorization on cupom controller

--- finance-mongo/src/controllers/CupomController.js
+++ finance-mongo/src/controllers/CupomController.js
@@ -27,16 +27,20 @@ class CupomController {
 
     static createCupom = async (req, res) => {
         const ids = req.body;
+        const { authorization } = req.headers;
 
         try {
-            const userData = await axios.get(`http://localhost:3002/user/${ids.user_id}`);
+            const userData = await axios.get(`http://localhost:3002/user/${ids.user_id}`,
+                { headers: { 'Authorization': authorization } });
             const user_info = omit(userData.data, 
                 ['_id', 'email', 'password', 'phones', 'creation_date']);
 
-            const saleData = await axios.get(`http://localhost:3004/sale/${ids.sale_id}`);
+            const saleData = await axios.get(`http://localhost:3004/sale/${ids.sale_id}`,
+                { headers: { 'Authorization': authorization } });
             const sale_info = omit(saleData.data, ['_id', 'delivery_address']);
 
-            const paymentData = await axios.get(`http://localhost:3005/payment/${ids.payment_id}`);
+            const paymentData = await axios.get(`http://localhost:3005/payment/${ids.payment_id}`,
+                { headers: { 'Authorization': authorization } });
             const payment_info = omit(paymentData.data, ['_id', 'cvv', 'value']);
 
             const cupomInfo = { user_info, sale_info, payment_info };
