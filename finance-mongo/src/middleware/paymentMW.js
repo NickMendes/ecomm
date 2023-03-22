0e84122e64eacb112c8fc0a007979c6db440d3ab
Nick <marina@sirq.com.br>
2023-03-22 15:08:24 -0300

    fix: payjment joi middleware

--- finance-mongo/src/middleware/paymentMW.js
+++ finance-mongo/src/middleware/paymentMW.js
@@ -6,8 +6,7 @@ const validatePayment = (req, res, next) => {
         name: Joi.string().min(5).required(),
         card_number: Joi.string().creditCard().required(),
         expiration_date: Joi.string().pattern(/([\d]{4}-(0[1-9]|10|11|12)$)/).required(),
-        cvv: Joi.string().pattern(/[\d]{3}/).required(),
-        status: Joi.string().valid('CRIADO', 'CONFIRMADO', 'CANCELADO')
+        cvv: Joi.string().pattern(/[\d]{3}/).required()
     });
 
     const { error } = paymentSchema.validate(req.body);
